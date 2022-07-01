ef933458825747782a3d6c04c7061457bd0e4005
Long G <long.w.guan@gmail.com>
2022-06-30 23:48:43 -0700

    Add play again

--- app.js
+++ app.js
@@ -3,6 +3,63 @@ let playerScore = 0;
 let computerScore = 0;
 let tie = 0;
 
+// user clicks yes ==> BUTTONS SHOW UP
+const gameContainer = document.querySelector('#gameContainer');
+gameContainer.style.display = "none";
+
+const score = document.querySelector('#score');
+score.style.display = "none";
+
+const playAgain = document.querySelector('#playAgain');
+playAgain.style.display = "none";
+
+const results = document.querySelector('#results');
+results.style.display = "none";
+
+const intro = document.querySelector('#intro');
+const yesBtn = document.querySelector('#yes');
+yesBtn.addEventListener('click', function() {
+    hideOrShow(gameContainer);
+    hideOrShow(intro);
+    hideOrShow(score);
+});
+
+// toggles on or off the display of an element
+function hideOrShow(element) {
+    if (element.style.display === "none") {
+        element.style.display = "block";
+    } else {
+        element.style.display = "none";
+    }
+}
+
+// user clicks no ==> print message "AWWWW, MAYBE NEXT TIME :)"
+const noMessage = document.querySelector('#noMessage');
+noMessage.style.display = "none";
+const noBtn = document.querySelector('#no');
+noBtn.addEventListener('click', function() {
+    hideOrShow(noMessage);
+    hideOrShow(intro);
+});
+
+const rock = document.querySelector('#rock');
+const scissors = document.querySelector('#scissors');
+const paper = document.querySelector('#paper');
+
+rock.addEventListener('click', function() {
+    let playerSelection = this.id;
+    playRound(playerSelection, computerSelection());
+});
+scissors.addEventListener('click', function() {
+    let playerSelection = this.id;
+    playRound(playerSelection, computerSelection());
+});
+paper.addEventListener('click', function() {
+    let playerSelection = this.id;
+    playRound(playerSelection, computerSelection());
+});
+
+
 // Randomly generates num of 0, 1, or 2. 33% chance generator
 function randNum() {
     return Math.floor(Math.random() * 3);
@@ -11,64 +68,73 @@ function randNum() {
 // randomly returns either 'ROCK', 'PAPER', or 'SCISSORS'.
 // 0 = 'ROCK', 1 = 'PAPER', 2 = 'SCISSORS'
 function computerSelection() {
+    let computerMove = "";
     switch (randNum()) {
         case 0:
+            console.log('rock');
             return 'rock';
         case 1:
+            console.log('paper');
             return 'paper';
         case 2:
+            console.log('scissors')
             return 'scissors';
         default:
             console.log('Something went wrong!')
     }
 }
 
-// checks if the player's selection is valid and returns the valid choice
-function check(playerChoice) {
-    while (playerChoice != 'rock' && playerChoice != 'paper' && playerChoice != 'scissors') {
-        playerChoice = prompt('PLEASE ENTER A VALID SELECTION: ROCK, PAPER, OR SCISSORS?').toLowerCase();
-    }
-    return playerChoice;
+function updateScore() {
+    score.innerText = `PLAYER SCORE:${playerScore} | COMPUTER SCORE:${computerScore} | TIE:${tie}`;
+    hideOrShow(results);
+    hideOrShow(gameContainer);
+    hideOrShow(playAgain);
 }
 
-// allows the player select 'ROCK', 'PAPER', or 'SCISSOR' and returns the choice
-function playerSelection() {
-    let playerChoice = prompt('ROCK, PAPER, OR SCISSORS?').toLowerCase();
-    playerChoice = check(playerChoice);
-    return playerChoice;
+
+
+function playerWinProcedure() {
+    playerScore += 1;
+    updateScore();
 }
 
-// prints the player score, computer score, and number of ties
-function printScore() {
-    console.log(`PLAYER SCORE:${playerScore}; COMPUTER SCORE:${computerScore}; TIE:${tie}`);
+function computerWinProcedure() {
+    computerScore += 1;
+    updateScore();
 }
 
 // starts a single round of ROCK PAPER SCISSORS between player and computer, then prints out the winner of the round
 function playRound(playerSelection, computerSelection) {
     if (playerSelection == 'rock' && computerSelection == 'scissors') {
         console.log('YOU WIN! ROCK BEATS SCISSORS');
-        playerScore += 1;
+        playerWinProcedure();
     } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
         console.log('YOU WIN! SCISSORS BEATS PAPER');
-        playerScore += 1;
+        playerWinProcedure();
     } else if (playerSelection == 'paper' && computerSelection == 'rock') {
         console.log('YOU WIN! PAPER BEATS ROCK');
-        playerScore += 1;
+        playerWinProcedure();
     } else if (playerSelection == computerSelection) {
         console.log("IT'S A TIE");
         tie += 1;
+        updateScore();
     } else if (playerSelection == 'rock' && computerSelection == 'paper') {
         console.log('YOU LOSE! PAPER BEATS ROCK');
-        computerScore += 1;
+        computerWinProcedure();
     } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
         console.log('YOU LOSE! SCISSORS BEATS PAPER');
-        computerScore += 1;
+        computerWinProcedure();
     } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
         console.log('YOU LOSE! ROCK BEATS SCISSORS');
-        computerScore += 1;
-    } else (
-        console.log('SOMETHING WENT WRONG')
-    )
+        computerWinProcedure();
+    } else {
+        console.log(playerSelection);
+        console.log(computerSelection);
+        if (computerSelection == 'scissors') {
+            console.log('true');
+        }
+        console.log('SOMETHING WENT WRONG');
+    }
 }
 
 // loops the game in a for loop, number of games depend of user
